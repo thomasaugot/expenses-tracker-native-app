c62295533b9e067dbbae53c30d2d7edf61d9ffa0
Thomas Augot <thomas.augot@hotmail.fr>
2023-06-26 11:37:12 +0200

    adding screens, folders, installing navigation packages

---
+++ screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const ManageExpense = () => {
+  return <Text>Hello</Text>;
+};
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const RecentExpenses = () => {
+  return <Text>Hello</Text>;
+};
+
+export default RecentExpenses;
